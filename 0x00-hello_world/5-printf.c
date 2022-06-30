b0f8275eb697a2d69379d31fa7e4ff2d353c607a
ChristabelJ <bellajuma43@gmail.com>
2022-06-30 16:28:39 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main: Entry point
+ * main - used to print a string
  * Description: using main function
  * Return: always 0 (success)
  */
 int main(void)
-{	printf("with proper grammer, but the outcome is a piee of art;\n");
+{	printf("with proper grammer, but the outcome is a piece of art;\n");
 	return (0);
 }
