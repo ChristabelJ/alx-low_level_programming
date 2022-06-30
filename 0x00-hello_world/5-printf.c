79de21ef45c02e451b369a0a2be8cb5ea3dfdf11
ChristabelJ <bellajuma43@gmail.com>
2022-06-30 16:20:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main: Entry point
+ * Description: using main function
+ * Return: always 0 (success)
+ */
+int main(void)
+{	printf("with proper grammer, but the outcome is a piee of art;\n");
+	return (0);
+}
