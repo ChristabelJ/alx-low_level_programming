f013119c41ce7c9c2292c024d53d4ab0a1f0a774
ChristabelJ <bellajuma43@gmail.com>
2022-07-04 14:44:37 -0700

    advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,8 +9,10 @@ int main(void)
 {	int i, j;
 
 	for (j = 48 ; j < 58 ; j++)
-	{	for (i = j + 1 ; i < 58 ; i++)
-		{	putchar(j);
+	{
+		for (i = j + 1 ; i < 58 ; i++)
+		{
+			putchar(j);
 			putchar(i);
 			if (j != 56 || i != 57)
 			{	putchar(44);
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,13 @@ int main(void)
 {	int i, j, k;
 
 	for (j = 48 ; j < 58 ; j++)
-	{	for (i = j + 1 ; i < 58 ; i++)
-		{	for (k = i + 1 ; k < 58 ; k++)
-			{	putchar(j);
+
+	{
+		for (i = j + 1 ; i < 58 ; i++)
+		{
+			for (k = i + 1 ; k < 58 ; k++)
+			{
+				putchar(j);
 				putchar(i);
 				putchar(k);
 				if (j != 55 || i != 56 || k != 57)
