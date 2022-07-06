a6c56bce298a1956bc9fdd5603cd5277e67abde6
ChristabelJ <bellajuma43@gmail.com>
2022-07-06 09:44:01 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
-#include "holberton.h"
+#include main.h  
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for main.h
  * Return: Always 0.
  */
 int main(void)
 {
-	char holberton[] = "Holberton";
+	char main[] = "Main";
 	int i;
 
-	for (i = 0; holberton[i] != '\0'; i++)
+	for (i = 0; main[i] != '\0'; i++)
 	{
-		_putchar(holberton[i]);
+		_putchar(main[i]);
 	}
 	_putchar('\n');
 	return (0);
