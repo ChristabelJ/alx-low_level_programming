1dcc8e13c7e9202fc91d3f33b6f05a99fb647660
ChristabelJ <bellajuma43@gmail.com>
2022-07-06 07:56:53 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_alphabet - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letra = 'a';
+	char retorno = '\n';
+
+	while (letra <= 'z')
+	{
+		_putchar (letra);
+		letra++;
+	}
+	_putchar (retorno);
+}
