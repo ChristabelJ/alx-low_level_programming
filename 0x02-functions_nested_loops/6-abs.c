83e96be9a754c96c3553c0b009d70da430cc1b9f
ChristabelJ <bellajuma43@gmail.com>
2022-07-06 13:05:36 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include "main.h"
+/**
+ * _abs - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _abs(int c)
+{
+	return (abs(c));
+}
