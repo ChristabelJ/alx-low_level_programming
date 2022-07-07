b02f7806a162e20cc6bff70cbe5ff15bd23d0c80
ChristabelJ <bellajuma43@gmail.com>
2022-07-07 15:26:48 -0700

    more functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,24 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * _isupper - writes the character c to stdout
  * @c: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _isupper(int c)
-
 {
+int i;
 
-	int i;
-
-	if ((c >= 65 && c <= 90))
-	{
-		i = 1;
-	}
-	else
-	{
-		i = 0;
-	}
-	return (i);
+if ((c >= 65 && c <= 90))
+{
+	i = 1;
+}
+else
+{
+	i = 0;
+}
+return (i);
 }
