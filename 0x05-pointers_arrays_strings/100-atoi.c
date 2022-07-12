01b9973ef663610ad6bb7b71707eeece7d6faa02
ChristabelJ <bellajuma43@gmail.com>
2022-07-12 12:25:06 -0700

    convert str to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
  * _atoi - convert a string to an integer
  * @s: the string to be converted
  *
  * Return: The integer value of the converted string
  */
-
-int _atoi(char *)
+int _atoi(char *s)
 {
 	int sign = 1;
 	unsigned int num = 0;
@@ -15,13 +13,12 @@ int _atoi(char *)
 	do {
 		if (*s == '-')
 			sign *= -1;
-
 		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
+			num = (num * 10) + (*s - '0' );
 		else if (num > 0)
 			break;
-	} while (*s++);
+	}
+	while (*s++);
 
 	return (num * sign);
 }
