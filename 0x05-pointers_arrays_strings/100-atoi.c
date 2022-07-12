22d468f31afd2d6245f157cd50c964e679436b53
ChristabelJ <bellajuma43@gmail.com>
2022-07-12 12:58:35 -0700

    convert str to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,36 @@
 #include "main.h"
 /**
- * _atoi - convert a string to an integer
- * @s: the string to be converted
- *
- * Return: The integer value of the converted string
+ * _atoi - Convert a string to integer
+ * @s: char array string
+ * Return: first integer
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int num = 0;
+	int i;
 
-	do {
-		if (*s == '-')
-			sign *= -1;
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0' );
-		else if (num > 0)
-			break;
-	} while (*s++);
+	int h, p;
 
-	return (num * sign);
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			p *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			
+			else
+				h = (s[i] - '0') * -1;
+			
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+	}
+	if (p < 0)
+		h *= -1;
+
+	return (h);
 }
