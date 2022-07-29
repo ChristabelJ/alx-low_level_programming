4e2a71887ee28fba83118d857c4a78d5cd75940b
ChristabelJ <bellajuma43@gmail.com>
2022-07-28 23:08:06 -0700

    new task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
 
 /**
  * main - print the name of the program
- * argc: Count arguments
+ * @argc: Count arguments
  * @argv: Arguments
  *
  * Return: Always 0 (Success)
@@ -11,8 +11,8 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argv; /*Ignore argv*/
-	printf("%i\n", argc - 1);
-	
-	return (0);
+(void) argv; /*Ignore argv*/
+printf("%i\n", argc - 1);
+
+return (0);
 }
